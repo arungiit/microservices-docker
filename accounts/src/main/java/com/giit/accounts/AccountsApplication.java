3f7fd029e88459b24b6d2231877c820d9b48182d
arungiit <arun.kumar@giitian.com>
2024-06-10 11:39:13 +0530

    first commit

---
+++ accounts/src/main/java/com/giit/accounts/AccountsApplication.java
@@ -0,0 +1,35 @@
+package com.giit.accounts;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+@EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Accounts microservice REST API Documentation",
+				description = "giit Accounts microservice REST API Documentation",
+				version = "v1",
+				contact = @Contact(
+						name = "Arun kumar",
+						email = "arun@giitian.com.com",
+						url = "https://www.eazybytes.com"
+				),
+				license = @License(
+						name = "Apache 2.0",
+						url = "https://www.eazybytes.com"
+				)
+		)
+)
+public class AccountsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountsApplication.class, args);
+	}
+
+}
